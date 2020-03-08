2a2545c355a0c72acee1ea556d334ca6a4b0cdac
ShotaroOkada <b1016117@gmail.com>
2020-03-08 10:36:25 +0900

    create mongo driver

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/ShotaroOkada/Online_CA_Tech_Dojo_Golang
 
 go 1.13
+
+require go.mongodb.org/mongo-driver v1.3.1
---
+++ pkg/db/MongoDriver.go
@@ -0,0 +1,58 @@
+package mongo
+
+import (
+	"context"
+	"fmt"
+	"log"
+	"os"
+	"time"
+
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
+)
+
+var (
+	_mongoURL      = os.Getenv("MONGO_URL")
+	_mongoDataBase = os.Getenv("MONGO_DATABASE")
+)
+
+// IMongoDriver is interface
+type IMongoDriver interface {
+	Database() *mongo.Database
+}
+
+// Driver is struct
+type Driver struct {
+	DB *mongo.Database
+}
+
+// NewMongoDriver is func
+func NewMongoDriver() IMongoDriver {
+	result := &Driver{}
+	if err := result.EstablishConnection(); err != nil {
+		log.Fatal(err)
+	}
+	return result
+}
+
+// Database is func
+func (d *Driver) Database() *mongo.Database {
+	return d.DB
+}
+
+// EstablishConnection is func
+func (d *Driver) EstablishConnection() error {
+	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+	if cancel != nil {
+		return fmt.Errorf("driver.EstablishConnection() on WithTimeout")
+	}
+	client, err := mongo.NewClient(options.Client().ApplyURI(_mongoURL))
+	if err != nil {
+		return fmt.Errorf("driver.EstablishConnection() on NewClient:%w", err)
+	}
+	if err := client.Connect(ctx); err != nil {
+		return fmt.Errorf("driver.EstablishConnection() on Connect:%w", err)
+	}
+	d.DB = client.Database(_mongoDataBase)
+	return nil
+}
